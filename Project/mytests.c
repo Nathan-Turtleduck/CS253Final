efa07dc2dcdb2714a91a826e9f98e9dc4fb4af9c
Nathan Maroko <nathanmaroko@u.boisestate.edu>
2020-11-30 17:41:05 -0800

    Tests Successfully Written and Working

--- Project/mytests.c
+++ Project/mytests.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "ProcEntry.h"
 
-int testCreateProcEntry(){
+int testCreateProcEntry(void){
    char testName[] = "Test Create Proc Default/Destroy";
    ProcEntry * test = CreateProcEntry();
 
@@ -19,16 +19,12 @@ int testCreateProcEntry(){
 
 }
 
-int testCreateFromFile(){
+int testCreateFromFile(void){
    char testName[] = "Test Create Proc From File/Destroy";
 
-   FILE * testFile = fopen("file.txt", "w+");
-   fputs("1 (systemd) S 0 1 1 0", testFile);
+   char fileName[] = "/proc/1/stat";
 
-   rewind(testFile);
-   fclose(testFile);
-
-   ProcEntry * newEntry = CreateProcEntryFromFile("file.txt");
+   ProcEntry * newEntry = CreateProcEntryFromFile(fileName);
 
    if(newEntry == NULL){
       fprintf(stderr, "%s has failed\n", testName); 
@@ -41,12 +37,87 @@ int testCreateFromFile(){
    return 0;
 }
 
+int testCreatePrintDestroy(void){
+   char testName[] = "Test Create Proc From File/Print/Destroy";
+
+   char fileName[] = "/proc/1/stat";
+
+   ProcEntry * newEntry = CreateProcEntryFromFile(fileName);
+
+   if(newEntry == NULL){
+      fprintf(stderr, "%s has failed\n", testName); 
+      return 1;
+   }
+
+   PrintProcEntry(newEntry);
+   DestroyProcEntry(newEntry);
+   fprintf(stderr, "%s has passed\n", testName);
+
+   return 0;
+
+}
+
+int testCreateNULL(void){
+   char testName[] = "Test Create Proc From Null";
+
+   ProcEntry * nullEntry = CreateProcEntryFromFile(NULL);
+
+   if(nullEntry != NULL){
+      fprintf(stderr, "%s has failed\n", testName);
+      return 1;
+   }
+
+   fprintf(stderr, "%s has passed\n", testName);
+   return 0;
+}
+
+int testCreateFromFileDNE(void){
+   char testName[] = "Test Create Proc From File DNE";
+   
+   ProcEntry * dneEntry = CreateProcEntryFromFile("text.txt");
+
+   if(dneEntry != NULL){
+      fprintf(stderr, "%s has failed\n", testName);
+      return 1;
+   }
+
+   fprintf(stderr, "%s has passed\n", testName);
+   return 0;
+
+}
+
+int testCreateFromFileInvalid(void){
+   char testName[] = "Test Create Proc From File Invalid Format";
+
+   FILE * invalidFile = fopen("invalidFile.txt", "w+");
+   fputs("Invalid file cant get all values", invalidFile);
+
+   rewind(invalidFile);
+   fclose(invalidFile);
+
+   ProcEntry * invalidEntry = CreateProcEntryFromFile("invalidFile.txt");
+
+   if(invalidEntry != NULL){
+      fprintf(stderr, "%s has failed\n", testName);
+      return 1;
+   }
+
+   fprintf(stderr, "%s has passed\n", testName);
+   remove("invalidFile.txt");
+   return 0;
+
+}
+
 int main(void) {
 
    int testVals = 0;
 
    testVals += testCreateProcEntry();
    testVals += testCreateFromFile();
+   testVals += testCreatePrintDestroy();
+   testVals += testCreateNULL();
+   testVals += testCreateFromFileDNE();
+   testVals += testCreateFromFileInvalid();
 
 
    return testVals;
